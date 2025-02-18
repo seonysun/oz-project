7c2c11a2758d86a2365b74d430cd8c1d719d697f
seonysun <csh99240@naver.com>
2025-02-18 14:20:17 +0900

    feat: Footer 컴포넌트 생성

---
+++ src/layout/Footer.jsx
@@ -0,0 +1,19 @@
+function Footer() {
+  return (
+    <div className="flex h-[744px] flex-col items-center gap-6 bg-[#181313] px-8 py-12 text-white md:h-[464px] md:items-start md:px-[80px] md:py-[104px] lg:px-[160px]">
+      <div className="flex w-full flex-col items-center justify-between gap-8 md:flex-row">
+        <div>logo</div>
+        <div>Services</div>
+        <div>Assistance to the buyer</div>
+      </div>
+      <div className="flex w-[173px] justify-between">
+        <div>twit</div>
+        <div>face</div>
+        <div>tiktok</div>
+        <div>insta</div>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
