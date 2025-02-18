92858881a50696b1a8c9c851334b5cff2dd85956
seonysun <csh99240@naver.com>
2025-02-18 13:16:08 +0900

    feat: 페이지 레이아웃 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,19 @@
+import { Route, Routes } from 'react-router-dom';
+import Footer from './layout/Footer';
+import Header from './layout/Header';
+import Homepage from './pages/HomePage';
+import Listpage from './pages/ItemListPage';
+
 function App() {
   return (
-    <>
-      <div className="text-red-600">tailwind test</div>
-    </>
+    <div className="mx-auto w-full max-w-[1440px]">
+      <Header />
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/list" element={<Listpage />} />
+      </Routes>
+      <Footer />
+    </div>
   );
 }
 
