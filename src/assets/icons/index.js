adbefb25e43f09817e1c100c2a01c6877362f915
seonysun <csh99240@naver.com>
2025-02-19 18:06:50 +0900

    feat: Header 컴포넌트 아이콘 적용

--- src/assets/icons/index.js
+++ src/assets/icons/index.js
@@ -1,1 +1,10 @@
+/* logo */
+export { default as LogoBlack } from './LogoBlack.svg';
+export { default as LogoWhite } from './LogoWhite.svg';
+/* header */
+export { default as Burger } from './Burger.svg';
 export { default as Search } from './Search.svg';
+/* userIcons */
+export { default as Favorite } from './Favorites.svg';
+export { default as Cart } from './Cart.svg';
+export { default as User } from './User.svg';
--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -1,14 +1,32 @@
+import { Link } from 'react-router-dom';
 import Navbar from './Navbar';
+import { LogoBlack, Favorite, Cart, User, Burger } from '../assets/icons/index';
 import SearchInput from '../components/Input/SearchInput';
 
 function Header() {
+  const userIcons = [
+    { src: Favorite, alt: 'Favorite' },
+    { src: Cart, alt: 'Cart' },
+    { src: User, alt: 'User' },
+  ];
+
   return (
     <>
       <header className="flex h-[64px] items-center justify-between border px-4 md:h-[88px] md:px-[80px] lg:px-[160px]">
-        <div>logo</div>
+        <Link to="/">
+          <img src={LogoBlack} alt="logo" />
+        </Link>
         <SearchInput />
-        <div className="sm:hidden">burger</div>
-        <div className="hidden sm:block">icons</div>
+        <button className="sm:hidden" type="button">
+          <img src={Burger} alt="menu" />
+        </button>
+        <nav className="hidden sm:block">
+          <div className="flex gap-4">
+            {userIcons.map((icon) => (
+              <img key={icon.alt} src={icon.src} alt={icon.alt} />
+            ))}
+          </div>
+        </nav>
       </header>
       <Navbar />
     </>
