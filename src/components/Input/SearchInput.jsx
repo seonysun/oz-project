adc340e0ea56b3c7d5be40ec51d7290f40892652
SeonHyeong CHOE <110527802+seonysun@users.noreply.github.com>
2025-02-19 19:31:33 +0900

    feat: Input 컴포넌트 작업 (#8)
    * feat: input 컴포넌트 작성(검색용)
    
    * feat: Header에 input 컴포넌트 추가
    
    * feat: Header 컴포넌트 아이콘 적용
    
    * design: input 컴포넌트 css 수정
    
    * feat: Navbar 컴포넌트 아이콘 적용
    
    * feat: Footer 컴포넌트 아이콘 적용

---
+++ src/components/Input/SearchInput.jsx
@@ -0,0 +1,33 @@
+import { useState } from 'react';
+import { Search } from '../../assets/icons/index';
+
+function SearchInput() {
+  const [inputValue, setInputValue] = useState('');
+
+  const searchSubmit = (e) => {
+    e.preventDefault();
+    if (!inputValue.trim) return;
+    setInputValue('');
+  };
+
+  return (
+    <div className="sm:mx-10 sm:grow">
+      <form
+        className="flex h-[40px] max-w-[310px] items-center gap-2 rounded-lg bg-[#F5F5F5] px-4 opacity-0 sm:opacity-100 md:h-[56px]"
+        onSubmit={searchSubmit}
+      >
+        <button type="submit">
+          <img src={Search} alt="search icon" />
+        </button>
+        <input
+          className="w-full bg-transparent outline-none"
+          placeholder="Search"
+          value={inputValue}
+          onChange={(e) => setInputValue(e.target.value)}
+        />
+      </form>
+    </div>
+  );
+}
+
+export default SearchInput;
--- src/layout/Navbar.jsx
+++ src/layout/Navbar.jsx
@@ -1,15 +1,34 @@
+import { Link } from 'react-router-dom';
+import {
+  Phones,
+  Computers,
+  SmartWatches,
+  Cameras,
+  Headphones,
+  Gaming,
+} from '../assets/icons/index';
+
 function Nav() {
+  const navList = [
+    { name: 'Phones', src: Phones },
+    { name: 'Computers', src: Computers },
+    { name: 'Smart Watches', src: SmartWatches },
+    { name: 'Cameras', src: Cameras },
+    { name: 'Headphones', src: Headphones },
+    { name: 'Gaming', src: Gaming },
+  ];
+
   return (
-    <div className="hidden md:block">
-      <div className="flex h-[48px] items-center justify-between bg-[#2E2E2E] px-4 text-white md:px-[80px] lg:px-[160px]">
-        <div>Phones</div>
-        <div>Computers</div>
-        <div>Smart Watches</div>
-        <div>Cameras</div>
-        <div>Headphones</div>
-        <div>Gaming</div>
+    <nav className="hidden md:block">
+      <div className="flex h-[48px] justify-between bg-[#2E2E2E] px-4 md:px-[80px] lg:px-[160px]">
+        {navList.map((nav) => (
+          <Link key={nav.name} to="/" className="flex items-center gap-2">
+            <img src={nav.src} alt={nav.name} className="size-6" />
+            <span className="text-sm text-white">{nav.name}</span>
+          </Link>
+        ))}
       </div>
-    </div>
+    </nav>
   );
 }
 
