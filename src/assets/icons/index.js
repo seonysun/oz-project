f714a93849d1c8e09b89f45f1dc3a75250870681
seonysun <csh99240@naver.com>
2025-02-19 18:13:26 +0900

    feat: Navbar 컴포넌트 아이콘 적용

--- src/assets/icons/index.js
+++ src/assets/icons/index.js
@@ -8,3 +8,10 @@ export { default as Search } from './Search.svg';
 export { default as Favorite } from './Favorites.svg';
 export { default as Cart } from './Cart.svg';
 export { default as User } from './User.svg';
+/* navbar */
+export { default as Phones } from './Phones.svg';
+export { default as Computers } from './Computers.svg';
+export { default as SmartWatches } from './SmartWatches.svg';
+export { default as Cameras } from './Cameras.svg';
+export { default as Headphones } from './Headphones.svg';
+export { default as Gaming } from './Gaming.svg';
