4c8af17adbdb50aa1b4c328b0223df249745a676
seonysun <csh99240@naver.com>
2025-02-19 17:47:16 +0900

    feat: Header에 input 컴포넌트 추가

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -1,14 +1,15 @@
 import Navbar from './Navbar';
+import SearchInput from '../components/Input/SearchInput';
 
 function Header() {
   return (
     <>
-      <div className="flex h-[64px] items-center justify-between border px-4 md:h-[88px] md:px-[80px] lg:px-[160px]">
+      <header className="flex h-[64px] items-center justify-between border px-4 md:h-[88px] md:px-[80px] lg:px-[160px]">
         <div>logo</div>
+        <SearchInput />
         <div className="sm:hidden">burger</div>
-        <div className="hidden sm:block">searchbar</div>
         <div className="hidden sm:block">icons</div>
-      </div>
+      </header>
       <Navbar />
     </>
   );
