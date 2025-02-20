a883b8e36a53bc2a829377fe77e8498c276a0fa5
seonysun <csh99240@naver.com>
2025-02-20 12:00:01 +0900

    fix: Navbar 리스트 키값 관련 에러 수정

--- src/layout/Navbar.jsx
+++ src/layout/Navbar.jsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { Link } from 'react-router-dom';
 import {
   Phones,
@@ -22,13 +23,13 @@ function Nav() {
     <nav className="hidden md:block">
       <div className="flex h-[48px] items-center justify-between bg-[#2E2E2E] text-sm text-white md:px-[80px] lg:px-[160px]">
         {navList.map((nav, idx) => (
-          <>
+          <React.Fragment key={nav.name}>
             {idx > 0 && <span>|</span>}
-            <Link key={nav.name} to="/" className="flex items-center gap-1">
+            <Link to="/" className="flex items-center gap-1">
               <img src={nav.src} alt={nav.name} className="size-6" />
               <span>{nav.name}</span>
             </Link>
-          </>
+          </React.Fragment>
         ))}
       </div>
     </nav>
