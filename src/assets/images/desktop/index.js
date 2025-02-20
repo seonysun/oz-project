4bf0f64ec20fecb5e63640bb1667b891d2210523
seonysun <csh99240@naver.com>
2025-02-20 20:26:43 +0900

    refactor: 배너 이미지 import 관련 파일 누락

---
+++ src/assets/images/desktop/index.js
@@ -0,0 +1,6 @@
+export { default as Airpods } from './airpods.png';
+export { default as Iphone } from './iphone.png';
+export { default as Macbook } from './macbook.png';
+export { default as Playstation } from './playstation.png';
+export { default as Vision } from './vision.png';
+export { default as Sale } from './sale.png';
