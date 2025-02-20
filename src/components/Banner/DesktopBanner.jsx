c2baca86255aa1140fc45b84ba66e932ca1525bc
seonysun <csh99240@naver.com>
2025-02-20 10:33:31 +0900

    refactor: 배너 이미지 import 방식 변경

--- src/components/Banner/DesktopBanner.jsx
+++ src/components/Banner/DesktopBanner.jsx
@@ -1,25 +1,27 @@
-import airpods from '../../assets/images/desktop/airpods.png';
-import iphone from '../../assets/images/desktop/iphone.png';
-import macbook from '../../assets/images/desktop/macbook.png';
-import playstation from '../../assets/images/desktop/playstation.png';
-import vision from '../../assets/images/desktop/vision.png';
+import {
+  Airpods,
+  Iphone,
+  Macbook,
+  Playstation,
+  Vision,
+} from '../../assets/images/desktop/index';
 
 function DesktopBanner() {
   return (
     <section>
       <div>
-        <img src={iphone} alt="Banner" />
+        <img src={Iphone} alt="banner" />
       </div>
       <div className="flex">
         <div className="flex-col">
-          <img src={playstation} alt="Banner" />
+          <img src={Playstation} alt="banner" />
           <div className="flex w-full">
-            <img src={airpods} alt="Banner" className="w-1/2" />
-            <img src={vision} alt="Banner" className="w-1/2" />
+            <img src={Airpods} alt="banner" className="w-1/2" />
+            <img src={Vision} alt="banner" className="w-1/2" />
           </div>
         </div>
         <div>
-          <img src={macbook} alt="Banner" />
+          <img src={Macbook} alt="banner" />
         </div>
       </div>
     </section>
