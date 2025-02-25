fdde7e730d11e34e1feb883f6b3805153440dfbb
seonysun <csh99240@naver.com>
2025-02-25 20:25:00 +0900

    refactor: 상품 카드리스트  컴포넌트 분리

--- src/pages/Home/Products.jsx
+++ src/pages/Home/Products.jsx
@@ -1,15 +1,15 @@
 import { useState } from 'react';
-import ProductCard from '../../components/Card/ProductCard';
+import ProductCards from '../../components/Card/ProductCards';
 import useFetch from '../../utils/hooks/useFetch';
 
-const menuList = [
+const MENU_ITEMS = [
   { name: 'New Arrival', id: 5 },
   { name: 'Bestseller', id: 2 },
   { name: 'Featured Product', id: 3 },
 ];
 
 const getUrl = (menu) => {
-  const category = menuList.find((el) => el.name === menu);
+  const category = MENU_ITEMS.find((el) => el.name === menu);
   return category
     ? `https://api.escuelajs.co/api/v1/categories/${category.id}/products`
     : null;
@@ -18,7 +18,7 @@ const getUrl = (menu) => {
 function MenuTabs({ selectedMenu, setSelectedMenu }) {
   return (
     <ul className="mb-6 flex gap-7 pl-[3.5%]">
-      {menuList.map((menu) => (
+      {MENU_ITEMS.map((menu) => (
         <li key={menu.id}>
           <button
             type="button"
@@ -34,29 +34,14 @@ function MenuTabs({ selectedMenu, setSelectedMenu }) {
 }
 
 function Products() {
-  const [selectedMenu, setSelectedMenu] = useState(menuList[0].name);
+  const [selectedMenu, setSelectedMenu] = useState(MENU_ITEMS[0].name);
   const url = getUrl(selectedMenu);
-  const { data = [] } = useFetch(url, { limit: 8, offset: 0 });
+  const { data = [], loading } = useFetch(url, { limit: 8, offset: 0 });
 
   return (
     <section className="flex flex-col px-2 py-12 sm:px-[8%] md:px-[60px]">
       <MenuTabs selectedMenu={selectedMenu} setSelectedMenu={setSelectedMenu} />
-      <div className="flex flex-wrap justify-center gap-6">
-        {data ? (
-          data.map((item) => (
-            <ProductCard
-              key={item.id}
-              src={item.images}
-              name={item.title}
-              detail={item.description}
-              price={item.price}
-              like={item.like}
-            />
-          ))
-        ) : (
-          <p className="w-full text-center text-gray-500">상품이 없습니다.</p>
-        )}
-      </div>
+      {loading ? <p>로딩중...</p> : <ProductCards productList={data} />}
     </section>
   );
 }
--- src/pages/ItemList/ItemListPage.jsx
+++ src/pages/ItemList/ItemListPage.jsx
@@ -1,55 +1,82 @@
 import { useParams } from 'react-router-dom';
-import ListProducts from './ListProducts';
 import { ArrowLeft } from '../../assets/icons';
+import ProductCards from '../../components/Card/ProductCards';
 import SearchFilter from '../../components/Filter/SearchFilter';
 import DropdownInput from '../../components/Input/DropdownInput';
+import useFetch from '../../utils/hooks/useFetch';
+
+const CATEGORY_MAP = {
+  phones: 1,
+  computers: 2,
+  'smart watches': 3,
+  cameras: 4,
+  headphones: 5,
+  gaming: 6,
+};
+const FILTER_ITEM = [
+  {
+    title: 'Brand',
+    items: [
+      { id: 1, name: 'Apple' },
+      { id: 2, name: 'Xiaomi' },
+      { id: 3, name: 'Poco' },
+    ],
+  },
+  {
+    title: 'Battery capacity',
+    items: [
+      { id: 4, name: 'Apple' },
+      { id: 5, name: 'Banana' },
+      { id: 6, name: 'Cheese' },
+      { id: 7, name: 'Dried Mango' },
+      { id: 8, name: 'Egg' },
+    ],
+  },
+  {
+    title: 'Screen type',
+    items: [{ id: 9, name: 'Orange' }],
+  },
+];
+
+function MenuTabs({ category }) {
+  return (
+    <nav className="flex items-center gap-5 p-3 font-medium text-[#a2a1a1]">
+      <span>Home</span>
+      <img src={ArrowLeft} alt="arrow" className="size-4" />
+      <span>Catalog</span>
+      <img src={ArrowLeft} alt="arrow" className="size-4" />
+      <span className="font-semibold text-black">{category}</span>
+    </nav>
+  );
+}
 
 function ItemListPage() {
   const { category } = useParams();
-  const filterItemList = [
-    {
-      title: 'Brand',
-      items: [
-        { id: 1, name: 'Apple' },
-        { id: 2, name: 'Xiaomi' },
-        { id: 3, name: 'Poco' },
-      ],
-    },
-    {
-      title: 'Battery capacity',
-      items: [{ id: 4, name: 'Apple' }],
-    },
-    {
-      title: 'Screen type',
-      items: [{ id: 5, name: 'Apple' }],
-    },
-  ];
+  const url = category
+    ? `https://api.escuelajs.co/api/v1/categories/${CATEGORY_MAP[category]}/products`
+    : null;
+  const { data = [], loading } = useFetch(url, { limit: 12, offset: 0 });
 
   return (
-    <div className="my-4 flex-col items-center space-y-4 sm:px-[10%]">
-      <nav className="flex items-center gap-5 p-3 text-[#a2a1a1]">
-        <span>Home</span>
-        <img src={ArrowLeft} alt="arrow" className="size-4" />
-        <span>Catalog</span>
-        <img src={ArrowLeft} alt="arrow" className="size-4" />
-        <span className="text-black">{category}</span>
-      </nav>
+    <div className="my-4 space-y-4 sm:px-[10%]">
+      <MenuTabs category={category} />
       <div className="flex gap-4">
         <ul>
-          {filterItemList.map((filterItem, idx) => (
+          {FILTER_ITEM.map((filterItem, idx) => (
             <li key={filterItem.title}>
               <SearchFilter filterItem={filterItem} firstFilter={idx === 0} />
             </li>
           ))}
         </ul>
         <section className="flex-1">
-          <div className="flex items-center justify-between">
+          <div className="mb-6 flex items-center justify-between">
             <p className="text-[#a2a1a1]">
-              Selected Products : <span className="text-black">85</span>
+              Selected Products :{' '}
+              <span className="text-black">{data && data.length}</span>
             </p>
             <DropdownInput text="By rating" />
           </div>
-          <ListProducts />
+          {loading ? <p>로딩중...</p> : <ProductCards productList={data} />}
         </section>
       </div>
     </div>
