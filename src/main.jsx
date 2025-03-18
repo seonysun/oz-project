7817e6b6d80d865f244a2a8e63a04532378f7df5
seonysun <csh99240@naver.com>
2025-03-18 11:36:26 +0900

    chore: react-query 버전 변경(재설치)

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,5 @@
 import { StrictMode } from 'react';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { RouterProvider } from 'react-router-dom';
@@ -6,10 +7,13 @@ import { store } from './redux/store';
 import { router } from './utils/router';
 import './main.css';
 
+const queryClient = new QueryClient();
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider store={store}>
-      <RouterProvider router={router} />
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+      </QueryClientProvider>
     </Provider>
   </StrictMode>,
 );
